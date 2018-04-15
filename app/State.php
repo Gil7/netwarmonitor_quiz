cac618ed513765668ddb5a067b99cc1d2efd49ae
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-04-15 16:39:35 -0700

    :computer: State  model updated, relationships added with municipality  and contact models

---
+++ app/State.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class State extends Model
+{
+    protected $table = 'states';
+
+    protected $fillable = [
+        'name'
+    ];
+    public function municipalities(){
+        return $this->hasMany('App\Municipality');
+    }
+    public function conacts(){
+        return $this->hasMany('App\Contact');
+    }
+}
