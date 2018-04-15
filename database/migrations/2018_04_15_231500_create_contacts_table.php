76bf1f3fc82f8c32eafe21d87d2d3eca623d261c
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-04-15 16:45:00 -0700

    duplicated column on migration for contacts deleted

--- database/migrations/2018_04_15_231500_create_contacts_table.php
+++ database/migrations/2018_04_15_231500_create_contacts_table.php
@@ -17,7 +17,6 @@ class CreateContactsTable extends Migration
             $table->increments('id');
             $table->string("name");
             $table->string("lastname");
-            $table->string("lastname");
             $table->string("email");
             $table->string("phone");
             //foreigns keys
