471c4d89e581c8e64ab2c741f49c2f5fbc74272d
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-04-15 16:35:10 -0700

    :computer: Contact  model with custom functiones created, relationships defined

---
+++ app/Contact.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Auth;
+class Contact extends Model
+{
+    protected $table = 'contacts';
+
+    protected $fillable = [
+        "name", "lastname", "phone", "email",'municipality_id','user_id'
+    ];
+
+    public function municipality(){
+        return $this->belongsTo('App\Municipality');
+    }
+    function user(){
+        return $this->belongsTo('App\User');
+    }
+    public function appointments(){
+        $this->hasMany('App\Appointment');
+    }
+    public static function getMyContacts(){
+        $current_user = Auth::user()->id;
+        $contacts = DB::select(DB::raw(
+            "SELECT c.*, s.name as state_name, m.name as municipality_name from contacts as c 
+            INER JOIN states as s on c.state_id = s.id
+            INNER JOIN municipalities as m on m.id = c.municipality_id 
+            WHERE c.user_id = $current_user
+            ORDER BY c.id DESC"
+        ));
+        return $contacts;
+    }
+}
