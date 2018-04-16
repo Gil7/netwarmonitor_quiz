6eb1a42528c86c682ea232511f35022c031f938f
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-04-15 18:12:53 -0700

    index file for contacts created, table added to display user's contacts

---
+++ resources/views/contacts/index.blade.php
@@ -0,0 +1,26 @@
+@extends("layouts.app")
+
+@section("content")
+    <button style="margin:15px" data-option="create" class="show_modal btn btn-primary" data-toggle="modal" data-target="#contact-modal">
+        <i class="fa fa-plus"></i> Argegar 
+    </button>
+    <table class="table table-hover table-striped" id="table_contacts">
+        <thead>
+            <tr>
+                <th><i class="fa fa-user"></i> Nombre</th>
+                <th><i class="fa fa-user"></i> Apellidos</th>
+                <th><i class="fa fa-envelope"></i> Email</th>
+                <th><i class="fa fa-telephone"></i> Telefono</th>
+                <th><i class="fa fa-info"></i> Estado</th>
+                <th><i class="fa fa-info"></i> Municipio</th>
+                <th><i class="fa fa-gear"></i> Opciones</th>
+            </tr>
+        </thead>
+
+</table>
+
+@endsection
+
+@section('extra-js')
+    <!--script src="/js/contacts.js"></script-->
+@endsection
